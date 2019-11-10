bcfdd7d86dd48cf34219c220a392731f6d819c2e
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-10 17:47:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  seconds = 10 
+ while seconds >= 1 
+   puts "#{seconds} SECOND(S)!"
+   seconds -= 1 
+ end
+ if seconds == 0 
+  return "HAPPY NEW YEAR!"
+ end
+end
+
+
+def countdown_with_sleep(seconds)
+   seconds = 10 
+ while seconds >= 1 
+   puts "#{seconds} SECOND(S)!"
+   seconds -= 1 
+   sleep(1)
+ end
+ if seconds == 0 
+  return "HAPPY NEW YEAR!"
+ end
 end
