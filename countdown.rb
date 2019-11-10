b60e7830b740d831cecc833408f1b54f4ef5d279
Whitney Hodgkins <hodgkins.whitney@gmail.com>
2019-11-10 17:42:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(seconds)
   while seconds >= 1 
   puts "#{seconds} SECOND(S)!"
   seconds -= 1 
-  when seconds == 10
-    return "HAPPY NEW YEAR!"
 end
+if seconds == 0 
+  return "HAPPY NEW YEAR!"
 end
